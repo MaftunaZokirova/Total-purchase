73554650ad2f9b108f5689f0061c3c14e779601f
Abdulboriy <abduboriy3444@gmail.com>
2024-06-24 21:36:45 -0500

    New folder

---
+++ main.py
@@ -0,0 +1,14 @@
+from totalpurchase import Totalpurchase
+
+def main():
+
+    
+    total_purchase = Totalpurchase(num_of_items = 5, price_per_item = 4.55)
+    total_purchase.Run()
+
+    
+
+
+if __name__ == "__main__":
+    main()
+    print( "Today i have to finish python project")
---
+++ totalpurchase.py
@@ -0,0 +1,31 @@
+class Totalpurchase:
+    def __init__(self, num_of_items: int, price_per_item: float ) -> None:
+        self.num_of_items = num_of_items
+        self.price_per_item = price_per_item
+        self.total_price = 0.0
+
+
+    def PromptNumofItem(self) -> None:
+        self.num_of_items = input("Enter the number of items purchased: ")
+        
+    def PromptpricePerItem(self) -> None:   
+        self.price_per_item = input ("Enter the price per item: $")
+        
+    def TotalCost(self, num_of_items: float, price_per_item: float ) -> float:
+        the_cost = 0.0
+        subtotal = 0.0
+        # Sales tax is 5% = 0.05
+        subtotal = float(num_of_items) * float(price_per_item)
+        the_cost = subtotal + subtotal * 0.05
+        
+        return the_cost
+        
+    def DisplayTotalCost(self) -> None:
+        
+        print("Total purchase price is :", self.total_price)
+
+    def Run(self) -> None:
+        
+        self.total_price = self.TotalCost(self.num_of_items, self.price_per_item)
+        self.DisplayTotalCost()
+
